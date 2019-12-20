c9abc28d9618e2b6568eed9a108e38610007b00b
dlomedes <diomedes.lajara@gmail.com>
2019-12-20 15:39:50 -0500

    completed mobile menu and navigation

--- dist/app.c3f9f951.js
+++ dist/app.c3f9f951.js
@@ -245,33 +245,38 @@ initSlide();
 },{}],"js/components/menu.js":[function(require,module,exports) {
 // select dom elements
 // // select dom items
-// const menuBtn = document.querySelector(".menuBtn");
-// const nav = document.querySelector(".nav");
-// const navMenu = document.querySelector(".navMenu");
-// let navLinks = document.querySelectorAll(".navMenuLink");
-// // set initial state of menu
-// let menuState = false;
-// // create function for event
-// toggleMenu = () => {
-//   if (!menuState) {
-//     menuBtn.classList.add("close");
-//     nav.classList.add("show");
-//     navMenu.classList.add("show");
-//     // loop through navmenu links
-//     navLinks.forEach(item => item.classList.add("show"));
-//     // set state for menu
-//     menuState = true;
-//   } else {
-//     // reverse toggle
-//     menuBtn.classList.remove("close");
-//     nav.classList.remove("show");
-//     navMenu.classList.remove("show");
-//     navLinks.forEach(item => item.classList.remove("show"));
-//     menuState = false;
-//   }
-// };
-// // add event to button
-// menuBtn.addEventListener("click", toggleMenu);
+var menuBtn = document.querySelector(".menuBtn");
+var menu = document.querySelector(".menu");
+var menuMobile = document.querySelector(".menuMobile");
+var mobileLinks = document.querySelectorAll(".menuMobile-link"); // set initial state of menu
+
+var menuState = false; // create function for event
+
+toggleMenu = function toggleMenu() {
+  if (!menuState) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuMobile.classList.add("show"); // loop through navmenu links
+
+    mobileLinks.forEach(function (item) {
+      return item.classList.add("show");
+    }); // set state for menu
+
+    menuState = true;
+  } else {
+    // reverse toggle
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuMobile.classList.remove("show");
+    mobileLinks.forEach(function (item) {
+      return item.classList.remove("show");
+    });
+    menuState = false;
+  }
+}; // add event to button
+
+
+menuBtn.addEventListener("click", toggleMenu);
 },{}],"js/app.js":[function(require,module,exports) {
 "use strict";
 
@@ -308,7 +313,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64936" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58337" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/components/menu.js
+++ src/js/components/menu.js
@@ -1,30 +1,29 @@
 // select dom elements
-
 // // select dom items
-// const menuBtn = document.querySelector(".menuBtn");
-// const nav = document.querySelector(".nav");
-// const navMenu = document.querySelector(".navMenu");
-// let navLinks = document.querySelectorAll(".navMenuLink");
-// // set initial state of menu
-// let menuState = false;
-// // create function for event
-// toggleMenu = () => {
-//   if (!menuState) {
-//     menuBtn.classList.add("close");
-//     nav.classList.add("show");
-//     navMenu.classList.add("show");
-//     // loop through navmenu links
-//     navLinks.forEach(item => item.classList.add("show"));
-//     // set state for menu
-//     menuState = true;
-//   } else {
-//     // reverse toggle
-//     menuBtn.classList.remove("close");
-//     nav.classList.remove("show");
-//     navMenu.classList.remove("show");
-//     navLinks.forEach(item => item.classList.remove("show"));
-//     menuState = false;
-//   }
-// };
-// // add event to button
-// menuBtn.addEventListener("click", toggleMenu);
+const menuBtn = document.querySelector(".menuBtn");
+const menu = document.querySelector(".menu");
+const menuMobile = document.querySelector(".menuMobile");
+let mobileLinks = document.querySelectorAll(".menuMobile-link");
+// set initial state of menu
+let menuState = false;
+// create function for event
+toggleMenu = () => {
+  if (!menuState) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuMobile.classList.add("show");
+    // loop through navmenu links
+    mobileLinks.forEach(item => item.classList.add("show"));
+    // set state for menu
+    menuState = true;
+  } else {
+    // reverse toggle
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuMobile.classList.remove("show");
+    mobileLinks.forEach(item => item.classList.remove("show"));
+    menuState = false;
+  }
+};
+// add event to button
+menuBtn.addEventListener("click", toggleMenu);
