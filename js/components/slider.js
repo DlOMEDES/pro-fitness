ff612fcb4d2d6d1d43ed2ba622045981c02c3a60
Diomedes <diomedes.lajara@gmail.com>
2020-01-07 06:50:24 -0500

    Updates

---
+++ js/components/slider.js
@@ -0,0 +1,49 @@
+// querySectorAll return a html collection like an array with index
+const slides = document.querySelectorAll(".slide"),
+  // target the arrows
+  arrowLeft = document.querySelector("#arrowLeft"),
+  arrowRight = document.querySelector("#arrowRight");
+// target current slide
+currentSlide = 0;
+
+// prep and clear all images
+resetImgs = () => {
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+};
+// initialize slide
+initSlide = () => {
+  resetImgs();
+  slides[0].style.display = "block";
+};
+
+// when left arrow is click show previous image
+arrowLeft.addEventListener("click", () => {
+  if (currentSlide === 0) {
+    currentSlide = slides.length;
+  }
+  prevImg();
+});
+// show previous image
+prevImg = () => {
+  resetImgs();
+  slides[currentSlide - 1].style.display = "block";
+  currentSlide--;
+};
+
+// when right arrow is clicked
+arrowRight.addEventListener("click", () => {
+  if (currentSlide === slides.length - 1) {
+    currentSlide = -1;
+  }
+  nextImg();
+});
+// show next
+nextImg = () => {
+  resetImgs();
+  slides[currentSlide + 1].style.display = "block";
+  currentSlide++;
+};
+// call initialized slide
+initSlide();
